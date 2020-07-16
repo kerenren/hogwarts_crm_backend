da42c6ac6b64b8b750ab1833e9bb855e06a73e45
Kelly <renkelei.kelly@gmail.com>
2020-07-17 01:00:01 +0300

    added validation for time and skill(skill anem, level) fields; curated return value in valid_user_exist

--- validators/valid_fileds_type.py
+++ validators/valid_fileds_type.py
@@ -3,10 +3,26 @@ class Validator:
         self.user_dict = user_dict
         # string_type = [user_dict["id"],user_dict["first_name"],user_dict["last_name"],user_dict["email"],user_dict["password"]]
 
-    def valid_fields_type(self):
-        for item in self.user_dict:
+    # when we create a user, the user_dict is coming from POST response json, thus the value of num should be num not string of num
+    def valid_skill_dict_vale(self, skill):
+        print(f"{skill}")
+        skills__list = self.user_dict[skill]
+        if len(skills__list) != 0:
+            for skill in skills__list:
+                if type(skill["name"]) != str:
+                    raise ValueError("Sorry, the skill name should be string")
+                if type(skill["level"]) != int:
+                    raise ValueError("Sorry, the skill level should be a number")
+
+    def valid_user_fields_type(self):
+        string_list = [self.user_dict["id"], self.user_dict["first_name"], self.user_dict["last_name"],
+                       self.user_dict["password"], self.user_dict["email"], self.user_dict["password"],
+                       self.user_dict["creation_time"], self.user_dict["last_updated_time"]]
+        for item in string_list:
             if type(item) != str:
                 raise ValueError("you fields is not a string")
+        self.valid_skill_dict_vale("existing_magic_skills")
+        self.valid_skill_dict_vale("desired_magic_skills")
 
     def valid_user_fields_exist(self):
         for value in self.user_dict.values():
@@ -19,66 +35,63 @@ class Validator:
             if isinstance(v, dict):
                 if self.user_dict["email"] in v.values():
                     return True
-            return False
+            return None
 
 
+hermione_dict = {
+    "id": "er4",
+    "first_name": "Hermione",
+    "last_name": "Granger",
+    "email": "hermione@hogwartsedu.com",
+    "password": "nnnn",
+    "creation_time": "07-16-20",
+    "last_updated_time": "rt",
+    "existing_magic_skills": [
+        {"name": "expanding",
+         "level": 5}
+    ],
+    "desired_magic_skills": [
+        {"name": "invisible", "level": 1},
+        {"name": "fly", "level": 5}
+    ]}
 
+all_user = {
+    "ishk234": {
+        "id": "er4",
+        "first_name": "Hermione",
+        "last_name": "Granger",
+        "email": "hermione@hogwartsedu.com",
+        "password": "nnnn",
+        "creation_time": "07-17-20",
+        "last_updated_time": "rt",
+        "existing_magic_skills": [
+            {"name": "expanding",
+             "level": 5}
+        ],
+        "desired_magic_skills": [
+            {"name": "invisible", "level": 1},
+            {"name": "fly", "level": 5}
+        ]},
+    "edft4d": {
+        "id": "er4",
+        "first_name": "Harry",
+        "last_name": "potter",
+        "email": "potter@hogwartsedu.com",
+        "password": "nnnn",
+        "creation_time": "07-16-20",
+        "last_updated_time": "07-17-20",
+        "existing_magic_skills": [
+            {"name": "expanding",
+             "level": 5}
+        ],
+        "desired_magic_skills": [
+            {"name": "invisible", "level": 1},
+            {"name": "fly", "level": 5}
+        ]}
+}
 
-#
-# hermione_dict = {
-#     "id": "er4",
-#     "first_name": "Hermione",
-#     "last_name": "Granger",
-#     "email": "hermione@hogwartsedu.com",
-#     "password": "nnnn",
-#     "creation_time": "07-16-20",
-#     "last_updated_time": "rt",
-#     "existing_magic_skills": [
-#         {"name": "expanding",
-#          "level": 5}
-#     ],
-#     "desired_magic_skills": [
-#         {"name": "invisible", "level": 1},
-#         {"name": "fly", "level": 5}
-#     ]}
-#
-# all_user = {
-#     "ishk234": {
-#         "id": "er4",
-#         "first_name": "Hermione",
-#         "last_name": "Granger",
-#         "email": "hermione@hogwartsedu.com",
-#         "password": "nnnn",
-#         "creation_time": "07-17-20",
-#         "last_updated_time": "rt",
-#         "existing_magic_skills": [
-#             {"name": "expanding",
-#              "level": 5}
-#         ],
-#         "desired_magic_skills": [
-#             {"name": "invisible", "level": 1},
-#             {"name": "fly", "level": 5}
-#         ]},
-#     "edft4d": {
-#         "id": "er4",
-#         "first_name": "Harry",
-#         "last_name": "potter",
-#         "email": "potter@hogwartsedu.com",
-#         "password": "nnnn",
-#         "creation_time": "07-16-20",
-#         "last_updated_time": "07-17-20",
-#         "existing_magic_skills": [
-#             {"name": "expanding",
-#              "level": 5}
-#         ],
-#         "desired_magic_skills": [
-#             {"name": "invisible", "level": 1},
-#             {"name": "fly", "level": 5}
-#         ]}
-# }
-#
-# validator = Validator(hermione_dict)
-# validator.valid_fields_type()
-# validator.valid_user_fields_exist()
-# is_exist = validator.valid_user_exist(all_user)
-# print(is_exist)
+validator = Validator(hermione_dict)
+validator.valid_user_fields_type()
+validator.valid_user_fields_exist()
+is_exist = validator.valid_user_exist(all_user)
+print(is_exist)
