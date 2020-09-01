f6ff7997c05dd34ad98e2e23bf7ce59173b36441
Kelly <renkelei.kelly@gmail.com>
2020-09-01 23:52:17 +0300

    removing sql statements"

--- sql_script/add_student.sql
+++
@@ -1,3 +0,0 @@
-INSERT INTO students (id,first_name, last_name, email, password, creation_time, last_updated_time)
-VALUES 
-(default,"jennifer", "Potter", "jennifer.potter@hogwarts.com", '2345ds',curdate(), CURRENT_TIMESTAMP);
